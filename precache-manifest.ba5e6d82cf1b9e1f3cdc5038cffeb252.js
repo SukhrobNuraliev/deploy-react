c77cdcf72d9e267b97fe0dc381e6688451b2e2d4
Sukhrob Nuraliev <sukhrobnuraliev@mail.ru>
2020-03-26 20:45:06 +0500

    first commit

---
+++ precache-manifest.ba5e6d82cf1b9e1f3cdc5038cffeb252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a574827ddd470e0422c4b076a2741210",
+    "url": "/index.html"
+  },
+  {
+    "revision": "697840cb827555e3f3a8",
+    "url": "/static/css/main.586f9095.chunk.css"
+  },
+  {
+    "revision": "169802dd746d269d3408",
+    "url": "/static/js/2.884a15b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.884a15b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "697840cb827555e3f3a8",
+    "url": "/static/js/main.9e7528cc.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
